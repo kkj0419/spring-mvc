b3a431b8a905c0fbf566bdac3a3e6165090c2ba1
kkj0419 <kkhi0419@gmail.com>
2022-10-05 17:15:29 +0900

    [refactor] 전송용 form 객체 분리 후 로직 수정

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV4.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV4.java
@@ -7,7 +7,6 @@ import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -18,8 +17,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
-import hello.itemservice.domain.item.SaveValidation;
-import hello.itemservice.domain.item.UpdateValidation;
+import hello.itemservice.web.validation.form.ItemSaveForm;
+import hello.itemservice.web.validation.form.ItemUpdateForm;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -49,49 +48,23 @@ public class ValidationItemControllerV4 {
 		return "validation/v4/addForm";
 	}
 
-	/**
-	 * @Validated(@Valid) 어노테이션을 통하여 LocalValidatorFactoryBean을 validator로 등록
-	 * <- 개발자가 따로 등록한 validator가 존재하지 않을 때
-	 */
-	// @PostMapping("/add")
-	public String addItem(@ModelAttribute @Validated Item item, BindingResult result,
-		RedirectAttributes redirectAttributes) {
-
-		//복합 rule(global error)
-		if (item.getPrice() != null && item.getQuantity() != null) {
-			int resultPrice = item.getPrice() * item.getQuantity();
-			if (resultPrice < 10000) {
-				result.addError(new ObjectError("item", "가격 * 수량의 합은 10,000원 이상이어야 합니다."));
-			}
-		}
-
-		//자동으로 model add
-		if (result.hasErrors()) {
-			return "validation/v4/addForm";
-		}
-
-		Item savedItem = itemRepository.save(item);
-		redirectAttributes.addAttribute("itemId", savedItem.getId());
-		redirectAttributes.addAttribute("status", true);
-		return "redirect:/validation/v4/items/{itemId}";
-	}
-
 	@PostMapping("/add")
-	public String addItemV2(@ModelAttribute @Validated(SaveValidation.class) Item item, BindingResult result,
+	public String addItem(@ModelAttribute("item") @Validated ItemSaveForm form, BindingResult result,
 		RedirectAttributes redirectAttributes) {
 
-		//복합 rule(global error)
-		if (item.getPrice() != null && item.getQuantity() != null) {
-			int resultPrice = item.getPrice() * item.getQuantity();
+		//(global error)
+		if (form.getPrice() != null && form.getQuantity() != null) {
+			int resultPrice = form.getPrice() * form.getQuantity();
 			if (resultPrice < 10000) {
-				result.addError(new ObjectError("item", "가격 * 수량의 합은 10,000원 이상이어야 합니다."));
+				result.reject("totalPriceMin", new Object[] {10000, resultPrice}, null);
 			}
 		}
 
-		//자동으로 model add
 		if (result.hasErrors()) {
 			return "validation/v4/addForm";
 		}
+
+		Item item = new Item(form.getItemName(), form.getPrice(), form.getQuantity());
 
 		Item savedItem = itemRepository.save(item);
 		redirectAttributes.addAttribute("itemId", savedItem.getId());
@@ -106,34 +79,15 @@ public class ValidationItemControllerV4 {
 		return "validation/v4/editForm";
 	}
 
-	// @PostMapping("/{itemId}/edit")
-	public String edit(@PathVariable Long itemId, @Valid @ModelAttribute Item item, BindingResult result) {
-
-		//복합 rule(global error)
-		if (item.getPrice() != null && item.getQuantity() != null) {
-			int resultPrice = item.getPrice() * item.getQuantity();
-			if (resultPrice < 10000) {
-				result.addError(new ObjectError("item", "가격 * 수량의 합은 10,000원 이상이어야 합니다."));
-			}
-		}
-
-		if (result.hasErrors()) {
-			return "validation/v4/editForm";
-		}
-
-		itemRepository.update(itemId, item);
-		return "redirect:/validation/v4/items/{itemId}";
-	}
-
 	@PostMapping("/{itemId}/edit")
-	public String editV2(@PathVariable Long itemId, @Validated(UpdateValidation.class) @ModelAttribute Item item,
+	public String edit(@PathVariable Long itemId, @Valid @ModelAttribute("item") ItemUpdateForm form,
 		BindingResult result) {
 
-		//복합 rule(global error)
-		if (item.getPrice() != null && item.getQuantity() != null) {
-			int resultPrice = item.getPrice() * item.getQuantity();
+		//(global error)
+		if (form.getPrice() != null && form.getQuantity() != null) {
+			int resultPrice = form.getPrice() * form.getQuantity();
 			if (resultPrice < 10000) {
-				result.addError(new ObjectError("item", "가격 * 수량의 합은 10,000원 이상이어야 합니다."));
+				result.reject("totalPriceMin", new Object[] {10000, resultPrice}, null);
 			}
 		}
 
@@ -141,9 +95,10 @@ public class ValidationItemControllerV4 {
 			return "validation/v4/editForm";
 		}
 
+		Item item = new Item(form.getItemName(), form.getPrice(), form.getQuantity());
+
 		itemRepository.update(itemId, item);
 		return "redirect:/validation/v4/items/{itemId}";
 	}
-
 }
 
