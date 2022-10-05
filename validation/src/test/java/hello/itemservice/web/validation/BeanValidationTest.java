a222c1a3a0aadcb4af7c72631afebdf74902da0a
kkj0419 <kkhi0419@gmail.com>
2022-10-05 15:13:40 +0900

    [test] Bean Validation 적용 테스트

---
+++ validation/src/test/java/hello/itemservice/web/validation/BeanValidationTest.java
@@ -0,0 +1,31 @@
+package hello.itemservice.web.validation;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.junit.jupiter.api.Test;
+
+import hello.itemservice.domain.item.Item;
+
+public class BeanValidationTest {
+
+	@Test
+	void beanValidationTest(){
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		Validator validator = factory.getValidator();
+
+		//공백(blank)
+		Item item = new Item("  ", 0, 10000);
+
+		Set<ConstraintViolation<Item>> violations = validator.validate(item);
+		for (ConstraintViolation<Item> violation : violations) {
+			System.out.println("violation = " + violation);
+			System.out.println("violation.message = " + violation.getMessage());
+		}
+
+	}
+}
