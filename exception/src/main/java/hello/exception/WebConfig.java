7947e6e7bc871812f14fe9556b2734b6c630ae60
kkj0419 <kkhi0419@gmail.com>
2022-10-23 23:03:20 +0900

    [add] LogInterceptor를 통한 예외처리 과정 실습

--- exception/src/main/java/hello/exception/WebConfig.java
+++ exception/src/main/java/hello/exception/WebConfig.java
@@ -6,15 +6,28 @@ import javax.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	@Bean
-	public FilterRegistrationBean logFilter(){
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LogInterceptor())
+			.order(1)
+			.addPathPatterns("/**")
+			.excludePathPatterns( //지정한 경로에 대하여 interceptor 미적용
+				"/css/**", "/*.ico",
+				"/error", "/error-page/**"
+			);
+	}
+
+	// @Bean
+	public FilterRegistrationBean logFilter() {
 		FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
 		filterRegistrationBean.setFilter(new LogFilter());
 		filterRegistrationBean.setOrder(1);
