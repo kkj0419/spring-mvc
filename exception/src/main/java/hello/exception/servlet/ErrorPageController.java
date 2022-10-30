f3bfb04a2a0721f2f63b5aa02155d4ee7e5a65e9
kkj0419 <kkhi0419@gmail.com>
2022-10-30 23:23:56 +0900

    [add] servlet을 이용한 api 예외 처리 실습

--- exception/src/main/java/hello/exception/servlet/ErrorPageController.java
+++ exception/src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -1,8 +1,16 @@
 package hello.exception.servlet;
 
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -34,6 +42,23 @@ public class ErrorPageController {
 		log.info("error-page 500");
 		printErrorInfo(request);
 		return "error-page/500";
+	}
+
+	/**
+	 * produces 설정 이용하여, Accept header가 Application/json일 때 해당 메서드가 호출되도록 함
+	 */
+	@RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Map<String, Object>> errorPage500Api(HttpServletRequest request,
+		HttpServletResponse response) {
+		log.info("API errorPage 500");
+
+		Map<String, Object> result = new HashMap<>();
+		Exception ex = (Exception)request.getAttribute(ERROR_EXCEPTION);
+		result.put("status", request.getAttribute(ERROR_STATUS_CODE));
+		result.put("message", ex.getMessage());
+
+		Integer statusCode = (Integer)request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		return new ResponseEntity<>(result, HttpStatus.valueOf(statusCode));
 	}
 
 	private void printErrorInfo(HttpServletRequest request) {
