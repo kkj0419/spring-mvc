853cd9c2871cd227d572b4b3b6677e29bc5f4ef9
kkj0419 <kkhi0419@gmail.com>
2022-10-31 18:46:27 +0900

    [refactor] controlleradvice 적용

--- exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -18,31 +18,6 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 public class ApiExceptionV2Controller {
 
-	//@ResponseStatus 이용하여 상태 코드 설정
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	@ExceptionHandler(IllegalArgumentException.class)
-	public ErrorResult illegalExHandle(IllegalArgumentException e) {
-		log.error("[exceptionHandle] EX ", e);
-		return new ErrorResult("BAD REQUEST", e.getMessage());
-	}
-
-	//@ExceptionHandler의 예외 생략 가능
-	@ExceptionHandler
-	public ResponseEntity<ErrorResult> userExHandle(UserException e) {
-		log.error("[exceptionHandle] EX ", e);
-		ErrorResult errorResult = new ErrorResult("USER - EX ", e.getMessage());
-		//ResponseEntity 이용한 상태 코드 설정
-		return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
-	}
-
-	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	@ExceptionHandler
-	//IllegalArgument, UserException 제외한 exception 처리
-	public ErrorResult exHandle(Exception e) {
-		log.error("[exceptionHandle] EX ", e);
-		return new ErrorResult("EX", "내부 오류");
-	}
-
 	@GetMapping("/api2/members/{id}")
 	public ApiExceptionController.MemberDTO getMember(@PathVariable("id") String id) {
 
